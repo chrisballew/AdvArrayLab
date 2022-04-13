cc2a8267a11f665fcef54ed531815cca8562579c
Chris <bluman325@gmail.com>
2022-04-13 15:21:59 -0500

    completed problem 6a

--- app.js
+++ app.js
@@ -218,6 +218,19 @@ console.log("Tomato and Cheese", TomatoandCheese)
 
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
+
+function problemSixA() {
+    let results = dishes.map(function(el){
+        for (i in el.cuisine)
+        return el.cuisine
+
+    })
+    return results
+
+}
+let Cuisines = problemSixA();
+console.log("Show Cuisines", Cuisines)
+
 // BONUS: (come back to this after finishing all)
 //6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
